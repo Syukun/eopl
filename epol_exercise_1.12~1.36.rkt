d2efaf4acc0d68f01b724b8f4d6709fb46485327
Syukun <33632061+Syukun@users.noreply.github.com>
2018-03-08 14:27:18 +0900

    Update and rename epol_exercise_1.15~1.36.rkt to epol_exercise_1.12~1.36.rkt

--- epol_exercise_1.12~1.36.rkt
+++ epol_exercise_1.12~1.36.rkt
@@ -1,4 +1,16 @@
 #lang eopl
+; Exercise 1.13
+(define subst-map
+  (lambda (new old slist)
+    (map (lambda (sexp) (subst-in-s-exp-map new old sexp)) slist)))
+(define subst-in-s-exp-map
+  (lambda (new old sexp)
+    (if (symbol? sexp)
+        (if (eqv? sexp old) new sexp)
+        (subst-map new old sexp))))
+
+
+
 ; Exercise 1.15
 ; duple : Int * SchemeValue -> list-of-scheme-value
 ; usage : returns a list containing n copies of x
